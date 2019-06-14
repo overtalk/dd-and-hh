22d29caae79e489366110a36faf1f80d65304f6d
秦翰 <qinhan@xindong.com>
2019-06-14 18:03:12 +0800

    Signed-off-by: 秦翰 <qinhan@xindong.com>

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ docker kill ddhh
 
 docker rm ddhh
 
-docker run --name ddhh -p 8081:80 -d -v /root/dd-and-hh:/usr/share/nginx/html  nginx
+docker run --name ddhh -p 80:80 -d -v /root/dd-and-hh:/usr/share/nginx/html  nginx
